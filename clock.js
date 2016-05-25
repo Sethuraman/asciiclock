825f4f096178287227370f2bb15e5cacddb8260c
Sethu <writetosethu@gmail.com>
2016-05-25 15:46:15 +1000

    making use of a typescript file.. just to learn

--- clock.js
+++ clock.js
@@ -1,57 +1,52 @@
-function getTimeFromCommandLine(){
-	debug("Please enter the time in the format hh:mm");
-	return readline();
+var Time = (function () {
+    function Time(hourAndMinute) {
+        var timeArray = hourAndMinute.split(":");
+        this.hour = parseInt(timeArray[0]);
+        this.minute = parseInt(timeArray[1]);
+    }
+    return Time;
+}());
+function getTimeFromCommandLine() {
+    debug("Please enter the time in the format hh:mm");
+    return readline();
 }
-
-
-function splitHourAndMinute(time) {
-	var timeArray = time.split(":");
-	return {
-		'hour': parseInt(timeArray[0]),
-		'minute': parseInt(timeArray[1])
-	}
+function splitHourAndMinute(hourAndMinute) {
+    return new Time(hourAndMinute);
 }
-
-function checkHourMatches(hour, index){
-	var twelveHourMod = hour % 12;
-	return twelveHourMod === index;
+function checkHourMatches(hour, index) {
+    var twelveHourMod = hour % 12;
+    return twelveHourMod === index;
 }
-
-function checkMinuteMatches(minute, index){
-	var minuteInFive = Math.floor(minute / 5);
-	return minuteInFive === index;
+function checkMinuteMatches(minute, index) {
+    var minuteInFive = Math.floor(minute / 5);
+    return minuteInFive === index;
 }
-
-function convertToTimeDisplayCharacter(isHour, isMinute){
-	if(isHour && isMinute) return 'x';
-	else if(isHour) return 'h';
-	else if(isMinute) return 'm';
-	return 'o';
+function convertToTimeDisplayCharacter(isHour, isMinute) {
+    if (isHour && isMinute)
+        return 'x';
+    else if (isHour)
+        return 'h';
+    else if (isMinute)
+        return 'm';
+    return 'o';
 }
-
-function createTimeArray(timeObject){
-	var timeArray = [];
-	for(var index = 0; index<12; index++){
-		var isHour = checkHourMatches(timeObject.hour, index);
-		var isMinute = checkMinuteMatches(timeObject.minute, index);
-		timeArray.push(convertToTimeDisplayCharacter(isHour, isMinute));  
-	}
-	return timeArray;
+function createTimeArray(timeObject) {
+    var timeArray = [];
+    for (var index = 0; index < 12; index++) {
+        var isHour = checkHourMatches(timeObject.hour, index);
+        var isMinute = checkMinuteMatches(timeObject.minute, index);
+        timeArray.push(convertToTimeDisplayCharacter(isHour, isMinute));
+    }
+    return timeArray;
 }
-
-function print(timeArray){
-	for(var index=0;index<7;index++){
-		if(index === 0 || index ===6){
-			debug("    "+timeArray[index]+"    ");
-		}else{
-			debug(timeArray[12-index]+"       "+timeArray[index])
-		}
-
-	}
+function print(timeArray) {
+    for (var index = 0; index < 7; index++) {
+        if (index === 0 || index === 6) {
+            debug("    " + timeArray[index] + "    ");
+        }
+        else {
+            debug(timeArray[12 - index] + "       " + timeArray[index]);
+        }
+    }
 }
-
 print(createTimeArray(splitHourAndMinute(getTimeFromCommandLine())));
-
-
-
-
